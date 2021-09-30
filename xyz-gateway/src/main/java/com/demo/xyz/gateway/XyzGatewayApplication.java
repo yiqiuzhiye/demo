6eea4807ca14dfa1fb40cc8d77e0595d8280901c
jiahong.li <@q2238552>
2021-09-30 17:31:08 +0800

    初始化

---
+++ xyz-gateway/src/main/java/com/demo/xyz/gateway/XyzGatewayApplication.java
@@ -0,0 +1,9 @@
+package com.demo.xyz.gateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class XyzGatewayApplication {
+}
